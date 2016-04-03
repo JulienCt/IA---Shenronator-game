92053ebfe8f7869c7e125d92f1be98be22299ade
Julien Catteau <j.catteau34@gmail.com>
2016-04-03 16:26:06 +0200

    Initial Commit

---
+++ Shenronator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shenronator
+//
+//  Created by Julien Catteau on 03/04/16.
+//  Copyright © 2016 Julien Catteau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
